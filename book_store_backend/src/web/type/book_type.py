05f888bc70d29d2f3b0c305845456fe8586763bd
thinpyai <thinpyaiwin@gmail.com>
2023-10-01 16:09:05 +0630

    Add doc.

--- book_store_backend/src/web/type/book_type.py
+++ book_store_backend/src/web/type/book_type.py
@@ -5,6 +5,7 @@ import strawberry
 
 @strawberry.type
 class BookType():
+    """Book schema object type"""
     name: str
     author: str
     published_date: str
@@ -14,6 +15,7 @@ class BookType():
 
 @strawberry.type
 class BookDetailType(BookType):
+    """Book detail schema object type"""
     edition_no: int = 1
     brief: Optional[str] = None
     author_intro: Optional[str] = None
@@ -21,9 +23,11 @@ class BookDetailType(BookType):
 
 @strawberry.type
 class BookListType():
+    """Book list schema object type"""
     books: List[BookType] = None
 
 
 @strawberry.input
 class BookDetailInput(BookDetailType):
+    """Book detail input schema object type"""
     id: str = None
